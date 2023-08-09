a68a37c21dc24127b4304864e1b1ca1a79d97649
jerd-dev <jonathanruizdublin@gmail.com>
2023-08-09 15:31:17 -0400

    barril de screens

---
+++ lib/screens/screens.dart
@@ -0,0 +1,6 @@
+export 'package:choppi_prueba/screens/character/character_detail_screen.dart';
+
+export 'package:choppi_prueba/screens/espisodes/episodes_screen.dart';
+
+export 'package:choppi_prueba/screens/character/characters_screen.dart';
+export 'package:choppi_prueba/screens/home/home_screen.dart';
